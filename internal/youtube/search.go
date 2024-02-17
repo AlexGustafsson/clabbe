9f6cab9175138650447bc497b4691a0cd75ca196
Alex Gustafsson <alex-gustafsson@live.se>
2024-02-17 09:23:35 +0100

    Add API key fetch

--- internal/youtube/search.go
+++ internal/youtube/search.go
@@ -12,16 +12,53 @@ import (
 	"regexp"
 )
 
+var innertubeApiKeyRegex = regexp.MustCompile(`"innertubeApiKey":"([^"]+)"`)
 var initialDataRegex = regexp.MustCompile(`var ytInitialData = (.*?)};`)
 
 type SearchClient struct {
+	apiKey string
 	client *http.Client
 }
 
-func NewSearchClient() *SearchClient {
-	return &SearchClient{
+func NewSearchClient(ctx context.Context) (*SearchClient, error) {
+	c := &SearchClient{
 		client: &http.Client{},
 	}
+	apiKey, err := c.FetchAPIKey(ctx)
+	if err != nil {
+		return nil, err
+	}
+	c.apiKey = apiKey
+	return c, nil
+}
+
+func (c *SearchClient) FetchAPIKey(ctx context.Context) (string, error) {
+	req, err := http.NewRequest(http.MethodGet, "https://www.youtube.com", nil)
+	if err != nil {
+		return "", err
+	}
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("unexpected status: %s", res.Status)
+	}
+
+	// Read just as little as is requried to match any API key
+	var buffer bytes.Buffer
+	reader := bufio.NewReader(io.TeeReader(res.Body, &buffer))
+	match := innertubeApiKeyRegex.FindReaderIndex(reader)
+	if match == nil {
+		return "", fmt.Errorf("unable to find API key in response")
+	}
+
+	// Extract the match
+	apiKey := buffer.String()[match[0]+19 : match[1]-1]
+
+	return apiKey, nil
 }
 
 func (c *SearchClient) Search(ctx context.Context, query string) ([]string, error) {
