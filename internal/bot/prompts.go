494bbdd07ca0a5e11661a79005d7ed50a92fe848
Alex Gustafsson <alex-gustafsson@live.se>
2024-10-19 14:10:59 +0200

    Restore basic prompt
    Restore the basic prompt functionality so that the bot works as
    intended.

--- internal/bot/prompts.go
+++ internal/bot/prompts.go
@@ -19,18 +19,51 @@ var defaultSongSuggestionTemplate string
 var defaultThemeSuggestionTemplate string
 
 func NewThemeRequest() *openai.CompletionRequest {
+	prompt := `Respond with themes for songs to include in a playlist. Respond with five themes, one theme per line, each containing at least four words. Don't include seasonal themes such as summer vibes or Christmas songs.
+Don't include the following words or similar words:
+Workout
+Empowerment
+Feel-good
+Study
+Love`
+
+	examples := strings.Split(`Underground indie rock vibes
+Classic rock ballads revisited
+West coast hip-hop classics
+Latin jazz fusion grooves
+Acoustic soulful folk tunes
+Underground indie rock vibes
+Classic rock ballads revisited
+West coast hip-hop classics
+Latin jazz fusion grooves
+Acoustic soulful folk tunes
+Synthwave retro cyber beats
+Reggae essentials for relaxation
+Indie pop anthems for road trips
+90s R&B slow jams sentiment
+High-energy EDM bangers
+Chill lo-fi hip-hop vibes
+Alternative rock anthems mix
+Country music storytelling tunes
+Jazzy swing dance classics`, "\n")
+	// Pick a few examples to include for context, providing a bit more random
+	// state to the model, which is otherwise quite deterministic
+	selectedExamples := make([]string, 5)
+	for i := 0; i < 5; i++ {
+		selectedExamples[i] = examples[rand.IntN(len(examples))]
+	}
 
 	return &openai.CompletionRequest{
 		Messages: []openai.Message{
 			{
-				Role: openai.RoleSystem,
-				// Content: prompt,
-				Content: "",
+				Role:    openai.RoleSystem,
+				Content: prompt,
+				// Content: "",
 			},
 			{
-				Role: openai.RoleAssistant,
-				// Content: strings.Join(selectedExamples, "\n"),
-				Content: strings.Join([]string{}, "\n"),
+				Role:    openai.RoleAssistant,
+				Content: strings.Join(selectedExamples, "\n"),
+				// Content: strings.Join([]string{}, "\n"),
 			},
 		},
 		Temperature: 1.2,
